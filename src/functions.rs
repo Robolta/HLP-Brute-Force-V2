085b750b02e2d7ece5f497ec09f10789c8c7228e
Robolta <ameliakgreenwood@gmail.com>
2023-05-19 11:14:33 -0400

    Moved mismapped index check due to fail frequency

--- src/functions.rs
+++ src/functions.rs
@@ -1,4 +1,4 @@
-use std::num::NonZeroUsize;
+use std::{num::NonZeroUsize, time::Instant};
 use itertools::Itertools;
 
 use crate::{constants::{STATES, ASCENDING, TARGET_DISTINCT, DEBUG, MISMAPS}, layers::{UNIQUE_LAYERS, Layer}, pairs::{VALID_PARENT_LAYERS, LAYER_PAIRS}};
@@ -20,8 +20,10 @@ impl Function {
     }
     
     pub fn next(&mut self, depth: usize) {
+        let start = Instant::now();
+
         if depth == self.layers.len() { // Increase depth
-            if DEBUG == 2 { println!("\t[Function.next] Increase depth to {}", depth + 1); }
+            if DEBUG == 3 { println!("\t[Function.next] +Increase depth to {} ({:?})", depth + 1, start.elapsed()); }
             self.layers.push(UNIQUE_LAYERS[VALID_PARENT_LAYERS[0]]);
             self.outputs.push(self.layers[depth - 1].output);
             return;
@@ -29,39 +31,39 @@ impl Function {
 
         let options = if depth == self.layers.len() - 1 { None } else { Some(&LAYER_PAIRS[self.layers[depth + 1].unique_index]) };
         if self.layers[depth].next(options) { // Successful layer iteration
-            if DEBUG == 3 { println!("\t[Function.next] Successful iteration"); }
+            //if DEBUG == 3 { println!("\t[Function.next] +Successful iteration ({:?})", start.elapsed()); }
             let input = if depth == self.layers.len() - 1 { ASCENDING } else { self.outputs[depth + 1] };
             self.outputs[depth] = self.layers[depth].pass(input);
             return;
         }
 
-        if DEBUG == 3 { println!("\t[Function.next] Iterate next layer"); }
         'next: loop {
+            if DEBUG == 3 { println!("\t[Function.next] -Iterate next layer ({:?})", start.elapsed()); }
             self.next(depth + 1); // Iterate next layer
 
             let parent_output = self.outputs[depth + 1];
 
-            if !VALID_PARENT_LAYERS.contains(&self.layers[depth + 1].unique_index) { // Parent validity check
-                if DEBUG == 3 { println!("\t[Function.next] Invalid parent layer"); }
-                continue;
-            }
-            if DEBUG == 3 { println!("\t[Function.next] Valid parent layer"); }
-
-            if parent_output.iter().unique().count() < TARGET_DISTINCT { // Distinct value check
-                if DEBUG == 3 { println!("\t[Function.next] Insufficient distinct values"); }
-                continue;
-            }
-            if DEBUG == 3 { println!("\t[Function.next] Sufficient distinct values"); }
-
             for (a, mismap_indices) in MISMAPS.iter().enumerate() { // Mismapped index check
                 for b in mismap_indices {
                     if parent_output[a] == parent_output[*b] {
-                        if DEBUG == 3 { println!("\t[Function.next] Mismapped values"); }
+                        if DEBUG == 3 { println!("\t[Function.next] -Mismapped values ({:?})", start.elapsed()); }
                         continue 'next;
                     }
                 }
             }
-            if DEBUG == 3 { println!("\t[Function.next] No mismapped values"); }
+            if DEBUG == 3 { println!("\t[Function.next] +No mismapped values ({:?})", start.elapsed()); }
+
+            if !VALID_PARENT_LAYERS.contains(&self.layers[depth + 1].unique_index) { // Parent validity check
+                if DEBUG == 3 { println!("\t[Function.next] -Invalid parent layer ({:?})", start.elapsed()); }
+                continue;
+            }
+            if DEBUG == 3 { println!("\t[Function.next] +Valid parent layer ({:?})", start.elapsed()); }
+
+            if parent_output.iter().unique().count() < TARGET_DISTINCT { // Distinct value check
+                if DEBUG == 3 { println!("\t[Function.next] -Insufficient distinct values ({:?})", start.elapsed()); }
+                continue;
+            }
+            if DEBUG == 3 { println!("\t[Function.next] +Sufficient distinct values ({:?})", start.elapsed()); }
 
             // Cache check
             let total_depth = self.layers.len();
@@ -69,27 +71,27 @@ impl Function {
             match self.cache.get(&parent_output) {
                 Some(best_depth) => { // Already in cache
                     if best_depth < &(true_depth) { // Worse than cache
-                        if DEBUG == 3 { println!("\t[Function.next] Worse than cache depth ({} < {} for {:?})", best_depth, true_depth, parent_output); }
+                        if DEBUG == 3 { println!("\t[Function.next] -Worse than cache depth ({} < {} for {:?}) ({:?})", best_depth, true_depth, parent_output, start.elapsed()); }
                         continue;
                     } else if best_depth > &(true_depth) { // Better than cache
-                        if DEBUG == 3 { println!("\t[Function.next] Better than cache depth ({} > {} for {:?})", best_depth, true_depth, parent_output); }
+                        if DEBUG == 3 { println!("\t[Function.next] +Better than cache depth ({} > {} for {:?}) ({:?})", best_depth, true_depth, parent_output, start.elapsed()); }
                         self.cache.put(parent_output, true_depth);
                     }
                 },
                 None => { // Not in cache
-                    if DEBUG == 3 { println!("\t[Function.next] Not in cache ({} for {:?})", true_depth, parent_output); }
+                    if DEBUG == 3 { println!("\t[Function.next] +Not in cache ({} for {:?}) ({:?})", true_depth, parent_output, start.elapsed()); }
                     self.cache.put(parent_output, true_depth);
                 },
             }
 
             // Handle current layer
             
-            if DEBUG == 3 { println!("\t[Function.next] Iterate current layer"); }
+            if DEBUG == 3 { println!("\t[Function.next] +Iterate current layer ({:?})", start.elapsed()); }
             self.layers[depth] = UNIQUE_LAYERS[LAYER_PAIRS[self.layers[depth + 1].unique_index][0]];
             self.outputs[depth] = self.layers[depth].pass(self.outputs[depth + 1]);
             break;
         }
 
-        if DEBUG == 3 { println!("\t[Function.next] Finished"); }
+        if DEBUG == 3 { println!("\t[Function.next] +Finished ({:?})", start.elapsed()); }
     }
 }
